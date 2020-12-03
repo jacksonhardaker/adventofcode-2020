de834d00bb7029a28f8e6f507f04f12ce2289ee5
Jackson Hardaker <jackson.hardaker@gmail.com>
2020-12-03 05:34:45 -0500

    day 3 part 1

--- day-3/toboggan-trajectory.ts
+++ day-3/toboggan-trajectory.ts
@@ -1,5 +1,15 @@
-export const findTreesByRight3Down1 = (input: Array<string>): number => {
-  return 0;
+export const findTreesByRight3Down1 = (map: Array<string>): number => {
+  const tree = "#";
+
+  let column = 0;
+  let trees = 0;
+  for (const row of map) {
+    if (row.charAt(column % row.length) === tree) {
+      trees++;
+    }
+    column += 3;
+  }
+  return trees;
 };
 
-export const part2 = () => {};
+export const part2 = (map: Array<string>) => {};
