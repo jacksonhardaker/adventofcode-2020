f0a69bbe024ad7636045430ca0feb29a474933b7
Jackson Hardaker <jackson.hardaker@gmail.com>
2020-12-10 20:41:25 -0500

    day 10 part 2 attempts

--- day-10/adapter-array.ts
+++ day-10/adapter-array.ts
@@ -1,11 +1,12 @@
-export const getJoltage = (input: Array<number>) => {
+export const getJoltage = (input: number[], initialJoltage = 0) => {
   const results = {
     jolts1: 0,
     jolts2: 0,
     jolts3: 0,
+    finalJoltage: 0,
   };
 
-  let currentJoltage = 0;
+  let currentJoltage = initialJoltage;
 
   while (true) {
     let finished = false;
@@ -30,6 +31,7 @@ export const getJoltage = (input: Array<number>) => {
     if (finished) {
       currentJoltage += 3;
       results.jolts3++;
+      results.finalJoltage = currentJoltage;
       break;
     }
   }
@@ -40,10 +42,86 @@ export const getJoltage = (input: Array<number>) => {
 /**
  * What is the number of 1-jolt differences multiplied by the number of 3-jolt differences?
  */
-export const part1 = (input: Array<number>) => {
+export const part1 = (input: number[]) => {
   const { jolts1, jolts3 } = getJoltage(input);
 
   return jolts1 * jolts3;
 };
 
-export const part2 = (input: Array<number>) => {};
+class Node {
+  value: number;
+  tree: Tree;
+  children: Node[];
+  parent: Node | null;
+
+  constructor(value: number, tree: Tree, parent: Node | null) {
+    this.value = value;
+    this.children = [];
+    this.tree = tree;
+    this.parent = parent;
+
+    let hasChildren = false;
+    [1, 2, 3].forEach((iterator) => {
+      if (tree.validValues.includes(value + iterator)) {
+        this.pushChild(value + iterator);
+        hasChildren = true;
+      }
+    });
+
+    if (!hasChildren) {
+      tree.leaves.push(this);
+    }
+  }
+
+  pushChild(value: number) {
+    this.children.push(new Node(value, this.tree, this));
+  }
+}
+
+class Tree {
+  root: Node;
+  validValues: number[];
+  leaves: Node[];
+
+  constructor(validValues: number[]) {
+    this.validValues = validValues;
+    this.leaves = [];
+    this.root = new Node(0, this, null);
+  }
+}
+
+const failure1 = (input: number[]) => {
+  // This solution uses too much space for the puzzle input.
+  // Would have been nice if we needed to keep track of the makeup of
+  // each combination.
+  const tree = new Tree(input);
+  return tree.leaves.length;
+}
+const failure2 = (input: number[]) => {
+  let totalCombinations = 0;
+  const traverse = (
+    input: number[],
+    currentJoltage: number,
+  ) => {
+    let hasNextStep = false;
+
+    [1, 2, 3].forEach((iterator) => {
+      if (input.includes(currentJoltage + iterator)) {
+        traverse(input, currentJoltage + iterator);
+        hasNextStep = true;
+      }
+    });
+
+    if (!hasNextStep) {
+      totalCombinations++;
+    }
+  };
+
+  traverse(input, 0);
+  return totalCombinations;
+}
+
+export const part2 = (input: number[]) => {
+  // return failure1(input);
+  // return failure2(input);
+};
--- day-10/day-10.test.js
+++ day-10/day-10.test.js
@@ -63,10 +63,59 @@ describe('Day 2 - Part 1', () => {
 })
 
 describe('Day 2 - Part 2', () => {
-  test('example input has x result', () => {
+  test('example input has 8 results', () => {
     const result = part2([
+      16,
+      10,
+      15,
+      5,
+      1,
+      11,
+      7,
+      19,
+      6,
+      12,
+      4,
     ])
 
-    expect(result).toEqual(undefined)
+    expect(result).toEqual(8)
+  })
+
+  test('example input has 19208 results', () => {
+    const result = part2([
+      28,
+      33,
+      18,
+      42,
+      31,
+      14,
+      46,
+      20,
+      48,
+      47,
+      24,
+      23,
+      49,
+      45,
+      19,
+      38,
+      39,
+      11,
+      1,
+      32,
+      25,
+      35,
+      8,
+      17,
+      7,
+      9,
+      4,
+      2,
+      34,
+      10,
+      3,
+    ])
+
+    expect(result).toEqual(19208)
   })
 })
