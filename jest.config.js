2e2998315f3edd3785208eadd640860cbd33cf92
Jackson Hardaker <jackson.hardaker@gmail.com>
2021-07-30 13:21:18 -0400

    test: add json-summary

--- jest.config.js
+++ jest.config.js
@@ -35,7 +35,7 @@ module.exports = {
   coverageProvider: "babel",
 
   // A list of reporter names that Jest uses when writing coverage reports
-  coverageReporters: ["json", "text", "lcov", "clover"],
+  coverageReporters: ["json-summary", "json", "text", "lcov", "clover"],
 
   // An object that configures minimum threshold enforcement for coverage results
   // coverageThreshold: undefined,
